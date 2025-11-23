366e0a832bd28f03ebdd5e6768839309a701f62e
Your Name <your-email@example.com>
2025-11-23 19:34:10 +0300

    исправил апи

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'
 
-const API_URL = 'http://localhost:5001/api'
+const API_URL = 'https://vetbackend-mby3.onrender.com/api'
 
 function App() {
   const [currentUser, setCurrentUser] = useState(() => {
